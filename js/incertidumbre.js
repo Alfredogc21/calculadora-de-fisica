ae9c4ece172df8b0e015c046f79f341c359a541b
Alfredo Gomez <alfretg.c@hotmail.com>
2024-05-29 01:35:16 -0500

    Correccion de la variable indeterminacion para recibir datos del input

--- js/incertidumbre.js
+++ js/incertidumbre.js
@@ -1,10 +1,13 @@
 // Constante de Principio de incertidumbre
-let indeterminacion = (1.0 * Math.pow(10, -6)).toExponential(1); // Constante de incertidumbre en m/s
-let partesIndeterminacion = indeterminacion.split("e");
-let parteDecimalIndeterminacion = partesIndeterminacion[0];
-let exponenteIndeterminacion = parseInt(partesIndeterminacion[1]);
 
-let masaElectron = (9.1 * Math.pow(10, -31)).toExponential(3); // Masa del electrón en kg
+let indeterminacion = 0;
+
+// let indeterminacion = (1.0 * Math.pow(10, -6)).toExponential(1); // Constante de incertidumbre en m/s
+// let partesIndeterminacion = indeterminacion.split("e");
+// let parteDecimalIndeterminacion = partesIndeterminacion[0];
+// let exponenteIndeterminacion = parseInt(partesIndeterminacion[1]);
+
+// let masaElectron = (9.1 * Math.pow(10, -31)).toExponential(3); // Masa del electrón en kg
 
 let constantePlanck = (6.63 * Math.pow(10, -34)).toExponential(3); // Constante de Planck en J s
 let partesConstantePlanck = constantePlanck.split("e");
@@ -22,6 +25,8 @@ let ax = 0, resultadosAx = 0, partesAx = 0, parteDecimalAx = 0, exponenteAx = 0;
 function calcular() {
     masaInput = parseFloat(document.getElementById('masa').value); // Masa del electrón en kg
     contenedorResultados = document.getElementById('results');
+    indeterminacion = parseFloat(document.querySelector("#velocidad").value); 
+
 
     if (isNaN(masaInput)) {
         contenedorResultados.innerHTML = "<p>Por favor, ingresa valores numéricos válidos.</p>";
@@ -31,28 +36,28 @@ function calcular() {
     //Operacion 1
     ap = (masaInput * indeterminacion).toExponential(3); // Incertidumbre de posición
     resultadosAp = ap + " kg m/s";
-    console.log(resultadosAp);
+    // console.log(resultadosAp);
 
     partesAp = ap.split("e");
     parteDecimalAp = partesAp[0];
     exponenteAp = parseInt(partesAp[1]);
-    console.log(parteDecimalAp);
-    console.log(exponenteAp);
+    // console.log(parteDecimalAp);
+    // console.log(exponenteAp);
 
     //Operacion 2
     ax = (constantePlanck) / (cuatroPi * ap);
     resultadosAx = ax.toExponential(3) + " m";
-    console.log(resultadosAx);
+    // console.log(resultadosAx);
 
     partesAx = resultadosAx.split("e");
     parteDecimalAx = partesAx[0];
     exponenteAx = parseInt(partesAx[1]);
-    console.log(parteDecimalAx);
-    console.log(exponenteAx);
+    // console.log(parteDecimalAx);
+    // console.log(exponenteAx);
 
 
     // Mostrar resultados iniciales
-    contenedorResultados.innerHTML = "<p>∆𝒑 = " + parteDecimalAp + "x 10 <sup> " + exponenteAp + "</sup> kg*m/s </p>" +
+    contenedorResultados.innerHTML = "<p>∆𝒑 = " + parteDecimalAp + " kg*m/s </p>" +
         "<p>∆𝑥 = " + parteDecimalAx +  "x 10 <sup> " + exponenteAx + " </p>" +
         "<button onclick='mostrarDetalle()'>Más detalle</button>";
 
@@ -71,13 +76,13 @@ function mostrarDetalle() {
         "</ul> <br>" +
 
         "<p><strong>Para calcular la incertidumbre del momento (∆𝑝):</strong></p>" +
-        "<p> ∆𝑝 = (" + masaInput + " kg) * (" + parteDecimalIndeterminacion + " x 10 <sup>" + exponenteIndeterminacion + "</sup> m/s)</p>" +
-        "<p> ∆𝑝 = " + parteDecimalAp + " x 10 <sup>" + exponenteAp + "</sup> kg m/s </p><br>" +
+        "<p> ∆𝑝 = (" + masaInput + " kg) * (" + indeterminacion + " m/s)</p>" +
+        "<p> ∆𝑝 = " + parteDecimalAp + " kg m/s </p><br>" +
 
         "<p><strong>Ahora se halla la incertidumbre mínima de la posición de la partícula  (∆𝑥):</strong></p>" + //Voy aca
         "<img src='imagenes/principioIncertidumbreCap.png' class='capPrincipioIncertidumbre' alt='Formula Principio Incertidumbre'>" +
         "<p> ∆𝑥 ≥ ℎ / (4π * ∆𝑝)</p>" + 
-        "<p> ∆𝑥 ≥ (" + parteDecimalConstantePlanck + " x 10 <sup>" + exponenteConstantePlanck + "</sup> J s) / (4π * " + parteDecimalAp + " x 10 <sup>" + exponenteAp + "</sup> kg m/s)</p>" +
+        "<p> ∆𝑥 ≥ (" + parteDecimalConstantePlanck + " x 10 <sup>" + exponenteConstantePlanck + "</sup> J s) / (4π * " + parteDecimalAp + " kg m/s)</p>" +
         "<p> ∆𝑥 ≥ " + parteDecimalAx + " x 10 <sup>" + exponenteAx + "</sup> m</p>";
 
         // "<p>Δλmax = λc * (1-cos(180°))</p>" +
